3a1f27885a49e0583b695f51f474a059ead04b04
kuttapat Somwang <Kuttapat Somwang>
2024-01-02 13:24:40 +0700

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { BrowserRouter, Route , Routes} from 'react-router-dom'
 
 function App() {
   return (
-      <>
-
-      </>
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<>
+              Landing Page
+            </>}/>
+        </Routes>
+     </BrowserRouter>
   )
 }
 
