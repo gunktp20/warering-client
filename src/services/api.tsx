717d59fecc711fe944c91c0d9027f9bb2687077a
sekkarin <644259021@webmail.npru.ac.th>
2024-03-17 18:10:17 +0700

    conf: config for build k8s

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -9,7 +9,7 @@ const api = axios.create({
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
