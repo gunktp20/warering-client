8c50c9a709cae793a6de2fc62760d2546c7af5f0
Ken Sekkarin <73205970+sekkarin@users.noreply.github.com>
2024-06-14 00:37:13 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   preview: {
-    port: 80,
-    host: '0.0.0.0'
-  },
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on mode in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the VITE_ prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(env.TEST);
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env.VITE_API_DOMAIN": JSON.stringify(
+        env.TEST
+      ),
+    },
+  };
 });
