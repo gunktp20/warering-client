becdd5c05ff2e8d82f6e37ef4707bca9707846f5
Ken Sekkarin <73205970+sekkarin@users.noreply.github.com>
2024-06-13 18:52:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+   define: {
+      VITE_EMQX_DOMAIN: process.env.VITE_EMQX_DOMAIN,
+      VITE_API_DOMAIN: process.env.VITE_API_DOMAIN,
+      VITE_EMQX_PROTOCAL: process.env.VITE_EMQX_PROTOCAL,
+      VITE_EMQX_HOST: process.env.VITE_EMQX_HOST,
+    },
 
   return defineConfig({
     plugins: [react()],
