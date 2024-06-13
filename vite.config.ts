072f3e7bf215c1d54544cdf996d56efd3256d111
Ken Sekkarin <73205970+sekkarin@users.noreply.github.com>
2024-06-13 23:43:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    VITE_EMQX_DOMAIN: process.env.VITE_EMQX_DOMAIN,
-    VITE_API_DOMAIN: process.env.VITE_API_DOMAIN,
-    VITE_EMQX_PROTOCAL: process.env.VITE_EMQX_PROTOCAL,
-    VITE_EMQX_HOST: process.env.VITE_EMQX_HOST
+   preview: {
+    port: 80,
+    host: '0.0.0.0'
   },
 });
