29da6cb56b897fd475b91e2958b127f0855c70e6
Ken Sekkarin <73205970+sekkarin@users.noreply.github.com>
2024-06-14 15:28:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
   // Load env file based on mode in the current working directory.
   // Set the third parameter to '' to load all env regardless of the VITE_ prefix.
-  const env = loadEnv(mode, process.cwd());
-  console.log("test ",env.TEST);
+  // const env = loadEnv(mode, process.cwd());
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+  console.log("test ",process.env);
   return {
     plugins: [react()],
     define: {
       "import.meta.env.VITE_API_DOMAIN": JSON.stringify(
-        env.VITE_API_DOMAIN
+        process.env.VITE_API_DOMAIN
       ),
     },
   };
