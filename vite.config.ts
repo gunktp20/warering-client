a0aaed49a2ea972ea6941045704a91ec7e4e8e61
sekkarin <644259021@webmail.npru.ac.th>
2024-06-14 18:05:53 +0700

    fig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-export default () => {
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "VITE_") };
+
   return defineConfig({
     plugins: [react()],
+    define: {
+      "import.meta.env.VITE_EMQX_DOMAIN": JSON.stringify(
+        process.env.VITE_EMQX_DOMAIN
+      ),
+
+      "import.meta.env.VITE_API_DOMAIN": JSON.stringify(
+        process.env.VITE_API_DOMAIN
+      ),
+      "import.meta.env.VITE_EMQX_PROTOCAL": JSON.stringify(
+        process.env.VITE_API_DOMAIN
+      ),
+      "import.meta.env.VITE_EMQX_HOST": JSON.stringify(
+        process.env.VITE_API_DOMAIN
+      ),
+    },
   });
 };
