ecc33dd19b51b8d271d6d594d3c0daa7c86c5353
sekkarin <644259021@webmail.npru.ac.th>
2024-06-14 18:02:37 +0700

    clear log

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,6 @@ import ApiKeys from "./pages/ApiKey";
 import ApiKeyInformation from "./pages/ApiKey/ApiKeyInformation";
 
 function App() {
-  console.log("hello");
-  console.log("process.env ",process.env.TZ)
-  console.log("import ",import.meta.env)
-  
   return (
     <BrowserRouter>
       <Routes>
