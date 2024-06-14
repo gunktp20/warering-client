23c09ae13869963fb999ffaa89623c3e0ea065c5
sekkarin <644259021@webmail.npru.ac.th>
2024-06-14 16:42:11 +0700

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   // Load env file based on mode in the current working directory.
   // Set the third parameter to '' to load all env regardless of the VITE_ prefix.
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     define: {
-      "import.meta.env.VITE_EMQX_DOMAIN": env.VITE_EMQX_DOMAIN
-        ? JSON.stringify(env.VITE_EMQX_DOMAIN)
-        : "http://13.229.135.29:8083/mqtt",
-
-      "import.meta.env.VITE_API_DOMAIN": env.VITE_API_DOMAIN
-        ? JSON.stringify(env.VITE_API_DOMAIN)
-        : "http://www.warering.online/api/",
-      "import.meta.env.VITE_EMQX_PROTOCAL": env.VITE_API_DOMAIN
-        ? JSON.stringify(env.VITE_API_DOMAIN)
-        : "ws",
-      "import.meta.env.VITE_EMQX_HOST": env.VITE_API_DOMAIN
-        ? JSON.stringify(env.VITE_API_DOMAIN)
-        : "13.229.135.29",
+      "import.meta.env.VITE_EMQX_DOMAIN": JSON.stringify(env.VITE_API_DOMAIN),
+      "import.meta.env.VITE_API_DOMAIN": JSON.stringify(env.VITE_API_DOMAIN),
+      "import.meta.env.VITE_EMQX_PROTOCAL": JSON.stringify(env.VITE_API_DOMAIN),
+      "import.meta.env.VITE_EMQX_HOST": JSON.stringify(env.VITE_API_DOMAIN),
     },
   };
 });
