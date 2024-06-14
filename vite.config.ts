05f0633c7aabc27adb83a8bccfeaed4051dd7620
sekkarin <644259021@webmail.npru.ac.th>
2024-06-14 18:01:57 +0700

    test last

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "VITE_") };
-
+export default () => {
   return defineConfig({
     plugins: [react()],
-    define: {
-      "import.meta.env.VITE_EMQX_DOMAIN": JSON.stringify(
-        process.env.VITE_EMQX_DOMAIN || "http://13.229.135.29:8083/mqtt"
-      ),
-
-      "import.meta.env.VITE_API_DOMAIN": JSON.stringify(
-        process.env.VITE_API_DOMAIN || "http://www.warering.online/api/"
-      ),
-      "import.meta.env.VITE_EMQX_PROTOCAL": JSON.stringify(
-        process.env.VITE_API_DOMAIN || "ws"
-      ),
-      "import.meta.env.VITE_EMQX_HOST": JSON.stringify(
-        process.env.VITE_API_DOMAIN || "13.229.135.29"
-      ),
-    },
-    preview: {
-      host: true,
-      port: 80,
-      strictPort: true,
-    },
   });
 };
